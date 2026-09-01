798500b05cd3221b14db2818b192ae842b1a58f0
Pablo Emilio Rappalini Mega <p.rappalini.laboral@gmail.com>
2026-09-01 10:53:31 -0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,14 +9,19 @@ data "aws_ami" "al2023" {
 }
 
 locals {
-  docker_root = "${path.module}/../docker"
+  docker_root           = "${path.module}/../docker"
+  prometheus_private_ip = cidrhost(var.public_subnet_cidr, 10)
+  loki_private_ip       = cidrhost(var.public_subnet_cidr, 11)
+  tempo_private_ip      = cidrhost(var.public_subnet_cidr, 12)
+  grafana_private_ip    = cidrhost(var.public_subnet_cidr, 13)
 }
 
 resource "aws_instance" "prometheus" {
   ami                         = data.aws_ami.al2023.id
   instance_type               = var.prometheus_instance_type
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.backend["prometheus"].id]
+  private_ip                  = local.prometheus_private_ip
+  vpc_security_group_ids      = [aws_security_group.prometheus.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2.name
   key_name                    = var.key_name
   associate_public_ip_address = true
@@ -31,6 +36,9 @@ resource "aws_instance" "prometheus" {
   user_data = templatefile("${path.module}/templates/bootstrap-prometheus.sh.tftpl", {
     compose_file      = base64encode(file("${local.docker_root}/prometheus/docker-compose.yml"))
     prometheus_config = base64encode(file("${local.docker_root}/prometheus/prometheus.yml"))
+    loki_ip           = local.loki_private_ip
+    tempo_ip          = local.tempo_private_ip
+    grafana_ip        = local.grafana_private_ip
   })
 
   metadata_options { http_tokens = "required" }
@@ -41,7 +49,8 @@ resource "aws_instance" "loki" {
   ami                         = data.aws_ami.al2023.id
   instance_type               = var.loki_instance_type
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.backend["loki"].id]
+  private_ip                  = local.loki_private_ip
+  vpc_security_group_ids      = [aws_security_group.loki.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2.name
   key_name                    = var.key_name
   associate_public_ip_address = true
@@ -66,7 +75,8 @@ resource "aws_instance" "tempo" {
   ami                         = data.aws_ami.al2023.id
   instance_type               = var.tempo_instance_type
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.backend["tempo"].id]
+  private_ip                  = local.tempo_private_ip
+  vpc_security_group_ids      = [aws_security_group.tempo.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2.name
   key_name                    = var.key_name
   associate_public_ip_address = true
@@ -91,6 +101,7 @@ resource "aws_instance" "grafana" {
   ami                         = data.aws_ami.al2023.id
   instance_type               = var.grafana_instance_type
   subnet_id                   = aws_subnet.public.id
+  private_ip                  = local.grafana_private_ip
   vpc_security_group_ids      = [aws_security_group.grafana.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2.name
   key_name                    = var.key_name
@@ -106,9 +117,9 @@ resource "aws_instance" "grafana" {
   user_data = templatefile("${path.module}/templates/bootstrap-grafana.sh.tftpl", {
     compose_file           = base64encode(file("${local.docker_root}/grafana/docker-compose.yml"))
     datasource_config      = base64encode(file("${local.docker_root}/grafana/provisioning/datasources/datasources.yml"))
-    prometheus_ip          = aws_instance.prometheus.private_ip
-    loki_ip                = aws_instance.loki.private_ip
-    tempo_ip               = aws_instance.tempo.private_ip
+    prometheus_ip          = local.prometheus_private_ip
+    loki_ip                = local.loki_private_ip
+    tempo_ip               = local.tempo_private_ip
     grafana_admin_user     = var.grafana_admin_user
     grafana_admin_password = var.grafana_admin_password
   })
